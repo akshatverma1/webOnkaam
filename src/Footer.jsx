cbe835ff8749283a489167fdb6a5d30fc8457c91
akshatverma1 <akshaverma14@gmail.com>
2024-10-08 01:23:19 +0530

    By Akshat

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import "./Footer.css";
 
 export default function Footerfunction(){
     return(
         <>
+        <div className="footerClass">
         <h4>Footer</h4>
+        </div>
+        
         </>
     )
 }
