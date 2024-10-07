2ea5e0ee6807a71646e3865f3af684b45213b1d3
akshatverma1 <akshaverma14@gmail.com>
2024-10-08 00:32:15 +0530

    By AKSHAT

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,7 +4,7 @@ export default function homepageFunction(){
     return(
         <div>
         <img src={"assets/ii.jpeg"} alt="akshat0"/>
-        <img src="\src\Logo.svg" alt="akshatjoijh"height="200px" width="200px"/>
+        <img src="\src\assets\Logo.svg" alt="akshatjoijh"height="200px" width="200px"/>
         <h4>
             Home Page
         </h4>
