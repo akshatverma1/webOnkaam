d10aaf1371a11a27296fee372ae42c6817a07e29
akshatverma1 <akshaverma14@gmail.com>
2024-10-07 23:53:39 +0530

    ASSET ADD BY AKSHAT

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export default function homepageFunction(){
     return(
         <>
+        <img src={"assets/ii.jpeg"} alt="akshat0"/>
+        <img src="/Onkaam/src/ii.jpeg" height={200} width={200}/>
         <h4>
             Home Page
         </h4>
