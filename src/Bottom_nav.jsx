c2980d23db3740c87b8eaa8dd3cc750e7bf5502a
akshatverma1 <akshaverma14@gmail.com>
2024-10-11 20:51:55 +0530

    Homepage Done By Akshat

--- src/Bottom_nav.jsx
+++ src/Bottom_nav.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./Bottom_nav.css"
 import "./Bottom_navigation.css"
-export default function bnav(){
-    return(
+
+
+let r = false;
+function change() {
+    r = true;
+}
+function classchange() {
+    r = false;
+}
+
+
+
+export default function bnav() {
+
+
+    return (
         <div className="footerClass">
-            <Link to="/ar" className='Icons'><i className="fas fa-vr-cardboard fa-lg"></i>
-            <p>AR</p>
+            <Link to="/ar" onClick={change} className='Icons'><i className="fas fa-vr-cardboard fa-lg"></i>
+                <p>AR</p>
             </Link>
-            <Link to="/booking" className='Icons'><i className="fas fa-clipboard-list fa-lg"></i>
-            <p>Booking</p></Link>
-            <Link to="/" className='Icons'>
-            <i className="fa-solid fa-house fa-lg"></i>
-            <p>Home</p></Link>
-            <Link to="/assistant" className='Icons'><i className="fas fa-comment-dots fa-lg"></i>
-            <p>Assistant</p></Link>
-            <Link to="/account" className='Icons'><i className="fas fa-user-tie fa-lg"></i>
-            <p>Account</p></Link>
-            </div>
+            <Link to="/booking" onClick={change} className='Icons'><i className="fas fa-clipboard-list fa-lg"></i>
+                <p>Booking</p></Link>
+            <Link to="/" className='Icons' onClick={classchange}>
+                {
+                    r == true ? (<i className="fa-solid fa-house-circle-check fa-lg"></i>) : (<i style={{ color: "red" }} className="fa-solid fa-house-circle-check fa-lg"></i>)
+                }
+                <p>Home</p></Link>
+            <Link to="/assistant" onClick={change} className='Icons'><i className="fas fa-comment-dots fa-lg"></i>
+                <p>Assistant</p></Link>
+            <Link to="/account" onClick={change} className='Icons'><i className="fas fa-user-tie fa-lg"></i>
+                <p>Account</p></Link>
+        </div>
+
     )
 }
